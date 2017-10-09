7839ce17dc4bd77c8644b6df164e0f7c94f96fcf
Mr.Viana <rafaelgov95@gmail.com>
2017-10-09 06:46:47 -0400

    up

--- Atividades/Projeto-Corrios-Angular2/front-end/src/app/shared/components/toast/ng2-toast-config.ts
+++
@@ -1,8 +0,0 @@
-import {ToastOptions} from 'ng2-toastr';
-
-export class CustomOption extends ToastOptions {
-  animate = 'flyRight'; // you can override any options available
-  newestOnTop = false;
-  showCloseButton = true;
-  preventDuplicates = true;
-}
--- Atividades/Projeto-Corrios-Angular2/front-end/src/app/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './pipes/shared-pipes.module';
-// export * from './components';
-export * from './guard/auth.guard';
-// export * from './services/autenticacao';
