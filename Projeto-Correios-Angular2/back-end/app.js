14275b14c517857d8af0b4ede7ec27461a3327d0
Mr.Viana <rafaelgov95@gmail.com>
2017-10-11 20:05:54 -0400

    up

--- Projeto-Correios-Angular2/back-end/app.js
+++ Projeto-Correios-Angular2/back-end/app.js
@@ -35,6 +35,70 @@ app.use(function (req, res, next) {
  * Rota padrão, com informaçõa do app e versão
  */
 
+app.get("/soap", function (req, res) {
+    var easysoap = require('easysoap');
+    
+        // define soap params
+        var params = {
+            host   : 'http://webservice.correios.com.br:80',
+            path   : '/service/rastro',
+            wsdl   : '/',
+    
+            // set soap headers (optional)
+            headers: [{
+                'name'     : 'item_name',
+                'value'    : 'item_value',
+                'namespace': 'item_namespace'
+            }]
+        }
+        var soapClient = easysoap.createClient(params);
+        
+        
+                /*
+                 * get all available functions
+                 */
+                soapClient.getAllFunctions()
+                    .then((functionArray) => { console.log(functionArray); })
+                    .catch((err) => { throw new Error(err); });
+        
+        
+                /*
+                 * get the method params by given methodName
+                 */
+                soapClient.getMethodParamsByName('buscaEventos')
+                    .then((methodParams) => {
+                        console.log(methodParams.request);
+                        console.log(methodParams.response);
+                    })
+                    .catch((err) => { throw new Error(err); });
+        
+        
+                /*
+                 * call soap method
+                 */
+                soapClient.call({
+                    method    : 'methodName',
+                    attributes: {
+                        xmlns: 'http://resource.webservice.correios.com.br'
+                    },
+                    params: {
+                        testParam: 1,
+                        testParam: [2, 3],
+                        testParam: {
+                            '_value'     : 4,
+                            '_attributes': {
+                                'xmlns1': 'http://www.sample.com/other'
+                            }
+                        }
+                    }
+                })
+                .then((callResponse) => {
+                    console.log(callResponse.data);	// response data as json
+                    console.log(callResponse.body);	// response body
+                    console.log(callResponse.header);  //response header
+                })
+                .catch((err) => { throw new Error(err); });
+});
 
 app.get("/info", function (req, res) {
     return res.status(HTTPStatus.OK).json({
